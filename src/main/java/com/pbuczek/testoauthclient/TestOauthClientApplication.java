cd5aebce954bc89bbde352a01688e7f4d8790a08
PawelBuczek <pawel.buczek@email.com>
2023-09-08 09:26:36 +0200

    Initial commit

---
+++ src/main/java/com/pbuczek/testoauthclient/TestOauthClientApplication.java
@@ -0,0 +1,13 @@
+package com.pbuczek.testoauthclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOauthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOauthClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pbuczek/testoauthclient/TestOauthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbuczek.testoauthclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOauthClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
